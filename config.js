bbe8891d11bdeccfca079e76718808787fcc323b
An Nguyen <tranminhannguyen@gmail.com>
2018-05-03 20:33:19 +0700

    init

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="'";
+config.admin.email="";
+config.app.name="TracNghiem";
+config.app.url="/";
+
+module.exports=config
